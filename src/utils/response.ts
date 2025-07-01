794e16ca6520abcade267eda29f44af20e0f0df8
Gustavo Haramura <gustavo.haramura@fdte.io>
2025-07-01 13:22:59 -0300

    feat: update response messages

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -12,6 +12,42 @@ export function successMessage(res: Response, msg: string, data?: Record<string,
   }
 }
 
+export function unauthorizedMessage(res: Response, msg: string, data?: Record<string, unknown>) {
+  try {
+    return res.status(401).send({
+      message: msg,
+      data
+    })
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
+
+export function conflictMessage(res: Response, msg: string, data?: Record<string, unknown>) {
+  try {
+    return res.status(409).send({
+      message: msg,
+      data
+    })
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
+
+export function unprocessableMessage(res: Response, msg: string, data?: Record<string, unknown>) {
+  try {
+    return res.status(422).send({
+      message: msg,
+      data
+    })
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
+
 export function errorMessage(msg: string, res: Response) {
   try {
     return res.status(500).send({
