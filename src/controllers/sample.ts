b8889e5b19900e991b71e9559e786c47f622c683
Gustavo Haramura <gustavo.haramura@fdte.io>
2025-07-01 18:54:21 -0300

    refactor: update from winston to aws powertools

--- src/controllers/sample.ts
+++ src/controllers/sample.ts
@@ -1,20 +1,25 @@
 import { SampleRepository } from "../repository/sample"
-import { logger } from "../utils/logger"
+import { instance } from "../utils/logger"
 import { errorMessage, successMessage } from "../utils/response"
 
 import type { Request, Response } from "express"
 
+const logger = instance("Controller")
 const repo = new SampleRepository()
 
 export async function getSampleById(req: Request, res: Response): Promise<void> {
-  logger.info("[getSampleById controller] called")
+  logger.info("getSampleById called")
   const { id } = req.body
+  logger.appendKeys({ id })
   try {
     const data = await repo.getSampleById(id)
+    logger.info('data fetched')
     successMessage(res, "sample fetched successfully", data)
+    logger.resetKeys();
   }
   catch (e) {
-    logger.error(e)
+    logger.error("error at getSampleById", e as Error)
     errorMessage(`${e}`, res)
+    logger.resetKeys();
   }
 }
