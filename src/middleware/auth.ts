f507666b4fc3e27eb7b4b8b57dc6b7b0a057ad0d
Gustavo Haramura <gustavo.haramura@fdte.io>
2025-07-01 18:38:28 -0300

    feat: add verification middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,37 @@
+// middlewares/auth.js
+import { CognitoJwtVerifier } from 'aws-jwt-verify';
+import { envs } from '../config/env';
+import type { Response, NextFunction } from 'express';
+import type { AuthenticatedRequest } from '../types/express';
+
+const verifier = CognitoJwtVerifier.create({
+  userPoolId: envs.COGNITO.POOL_ID,
+  tokenUse: 'access',
+  clientId: envs.COGNITO.CLIENT_ID,
+});
+
+export async function auth(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: 'Missing or invalid Authorization header' });
+    }
+    const token = authHeader.replace('Bearer ', '');
+
+    const payload = await verifier.verify(token);
+
+    req.user = {
+      sub: payload.sub,
+      clientId: payload.client_id,
+      iss: payload.iss,
+      usage: payload.token_use,
+      exp: payload.exp,
+      username: payload.username
+    };
+
+    next();
+  } catch (err) {
+    const errorMessage = err instanceof Error ? err.message : String(err);
+    return res.status(401).json({ message: 'Invalid or expired token', error: errorMessage });
+  }
+};
---
+++ src/types/express.ts
@@ -0,0 +1,12 @@
+import type { Request } from 'express';
+
+export interface AuthenticatedRequest extends Request {
+  user?: {
+    sub: string;
+    clientId: string;
+    iss: string;
+    usage: string;
+    exp: number;
+    username: string;
+  }
+}
